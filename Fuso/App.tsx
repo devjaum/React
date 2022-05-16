a40f39c44437d5bd46023737d072480256e89f75
Joao Carlos <61249758+devjaum@users.noreply.github.com>
2022-05-16 15:37:12 -0300

    Delete Fuso directory

--- Fuso/App.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Fuso from './scr/pages/tela-Fuso';
-
-export default function FusoHrr(){
-  return(
-    <Fuso/>
-  );
-}
